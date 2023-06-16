6beebfb193192c839b00b04cfb24aac15510c8e5
MineCoder77 <107895072+MineCoder77@users.noreply.github.com>
2023-06-16 17:27:58 +0300

    Update Utils.hpp

--- src/utils/header/Utils.hpp
+++ src/utils/header/Utils.hpp
@@ -9,24 +9,24 @@ class JsonConfig {
 private:
 	nlohmann::json json_config;
 	bool create_file();
-	const bool isset_config_path();
-	const bool isset_config();
+	bool isset_config_path();
+	bool isset_config();
 	void set_config();
 	static JsonConfig instance;
 	JsonConfig() {}
 	JsonConfig(const JsonConfig&) = delete;
 	JsonConfig& operator=(JsonConfig&) = delete;
-	const bool check_config_correct();
+	bool check_config_correct();
 public:
-	const void init();
+	void init();
 	static JsonConfig& get_instance();
 	nlohmann::json get_config();
 	bool contains_items();
 };
 
 namespace PluginFolder {
-	const bool contains_in_plugin_folder(std::string name);
+	bool contains_in_plugin_folder(std::string name);
 	void set_in_plugin_folder(std::string name);
 	void create_folder();
-	const bool isset_folder_path();
+	bool isset_folder_path();
 }
